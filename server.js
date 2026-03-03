1f22ad192c7e61e7742ff9aec76acdf357bd0d79
Chris Owen <c.ornine@gmail.com>
2026-03-03 06:14:20 +0000

    feat: add route with query params

--- server.js
+++ server.js
@@ -4,6 +4,38 @@ fastify.get('/', (reqq, res) => {
   res.send({hello: 'world'})
 })
 
+fastify.route({
+  method: 'GET',
+  url:'/test',
+  schema: {
+    // this is a parameter in the address, ie '?name=anyName'
+    querystring: {
+      type: 'object',
+      properties: {
+        name: {type: 'string'}
+      },
+      required: ['name']
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: {type: 'string'}
+        }
+      } 
+    }
+  },
+  preHandler: (req, res, done) => {
+    // logic here, runs before handler on every request
+    done()
+  },
+  handler: (request, reply) => {
+    console.log(request)
+    reply.send({hello: request.query.name})
+  }
+})
+
+
 fastify.listen({port: 3000}, (err) => {
   if (err) {
     fastify.log.error(err)
